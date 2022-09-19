a99edb099fc20c6fe3797afb8cebf795dc99716c
JulianRDev <JulianRDev@users.noreply.github.com>
2022-09-19 22:21:58 +0000

    Challenge One

---
+++ lab_input/lab_13_challenge.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   CHALLENGE - SHEBANG, INPUT, PRINT, CONCATENATE, VARIABLES """
+
+def main(): 
+
+    # collect name input from user
+    user_name = input("Hello there! What is your name?")
+
+    # collect day of week input from user
+    week_day = input("Do you know what day of the week it is?")
+
+    #display input back to user
+    print(f"Hello, {user_name}! Happy {week_day}!")
+
+main()
